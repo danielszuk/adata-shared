e0f74f1240e4f36c1af6a85ab2f007b11a0823ab
daniel.szuk <daniel.szuk@gmail.com>
2019-01-23 12:08:33 +0100

    fix(unit): domain implements dto

--- modules/unit/unit.domain.ts
+++ modules/unit/unit.domain.ts
@@ -1,6 +1,7 @@
 import { Length } from 'class-validator';
+import { IUnitDTO } from 'shared/modules/unit/unit.dto';
 
-export class UnitDomain {
+export class UnitDomain implements IUnitDTO {
   id: number;
 
   @Length(2, 63)
