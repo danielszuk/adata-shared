7398d4afda5322a251a21e49138f55b4434751a9
daniel.szuk <daniel.szuk@codelise.io>
2019-03-26 10:39:53 +0100

    feat(colors): add color palette

---
+++ enums/colors.enum.ts
@@ -0,0 +1,13 @@
+export enum Colors {
+  brown = '#8C3900',
+  red = '#FF001C',
+  orange = '#FF9819',
+  yellow = '#FFF700',
+  green = '#14CC3C',
+  blue = '#0D31FF',
+  blueCobalt = '#00ACFF',
+  purple = '#A823EB',
+  violet = '#6D2AD4',
+  grey = '#646464',
+  black = '#0A0A0A'
+}
