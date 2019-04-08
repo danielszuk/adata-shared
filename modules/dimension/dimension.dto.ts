e28d39df323e1fd6170df1b98b7edce0444ee14d
daniel.szuk <daniel.szuk@codelise.io>
2019-04-08 14:58:09 +0200

    feat(dimensions): add statistic properties

--- modules/dimension/dimension.dto.ts
+++ modules/dimension/dimension.dto.ts
@@ -2,4 +2,9 @@ export interface IDimensionDTO {
   id: number;
   name: string;
   unit?: string;
+
+  matricesByDim1?: number;
+  matricesByDim2?: number;
+  dbwbsByDim1?: number;
+  dbwbsByDim2?: number;
 }
