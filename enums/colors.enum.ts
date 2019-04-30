8ab35b91c27a963ccf76df8beccf3551ffa73baf
daniel.szuk <daniel.szuk@codelise.io>
2019-04-30 17:24:15 +0200

    fix(colors): yellow

--- enums/colors.enum.ts
+++ enums/colors.enum.ts
@@ -16,12 +16,12 @@ export const ColorCodes = {
   [Colors.brown]: '#8C3900',
   [Colors.red]: '#FF001C',
   [Colors.orange]: '#FF9819',
-  [Colors.yellow]: '#FFF700',
+  [Colors.yellow]: '#D5D040',
   [Colors.green]: '#14CC3C',
   [Colors.blue]: '#0D31FF',
   [Colors.blueCobalt]: '#00ACFF',
   [Colors.purple]: '#A823EB',
-  [Colors.violet]: '#6D2AD4',
-  [Colors.grey]: '#646464',
+  [Colors.violet]: '#5420A1',
+  [Colors.grey]: '#B0B0B0',
   [Colors.black]: '#0A0A0A'
 };
